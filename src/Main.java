b4e066e4d294b749f0ef082857f8c27cfcb51544
Kaminari <Opk26rus@yandex.ru>
2022-12-03 00:07:50 +0300

    test

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import manager.Manager;
+import tasks.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Manager manager = new Manager();
+        System.out.println("начало");
+        Task task1 = new Task("T1asdf", "asfas", TaskStatus.NEW);
+        Task task2 = new Task("T2as1111df", "as1111fas", TaskStatus.NEW);
+
+        Epic epic1 = new Epic("EP1as1111df", "as1111fas", TaskStatus.NEW);
+        Subtask sub1 = new Subtask("SUB1as1111df", "as1111fas", TaskStatus.NEW, epic1);
+        Subtask sub2 = new Subtask("SUB2as1111df", "as1111fas", TaskStatus.NEW,epic1);
+
+
+        Epic epic2 = new Epic("EP2a1333333333f", "as13333333333s", TaskStatus.NEW);
+        Subtask sub3 = new Subtask("SUB3as1111df", "as1111fas", TaskStatus.NEW, epic2);
+        manager.addTask(task1);
+        manager.addTask(task2);
+        manager.addEpic(epic1);
+        manager.addEpic(epic2);
+        manager.addSubask(sub1, epic1);
+        manager.addSubask(sub2, epic1);
+        manager.addSubask(sub3, epic2);
+        System.out.println(manager.epicAndCo(2));
+
+
+//  я не понял из задачи, нужно ли подтерать мои тесты, оставил как есть
+
+    }
+}
---
+++ src/tasks/Task.java
@@ -0,0 +1,77 @@
+package tasks;
+
+import java.util.Objects;
+
+public class Task {
+    protected String name;
+    protected String description;
+    protected int id;
+    protected TaskStatus status;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
+    public Task (String name, String description, TaskStatus status){
+        this.description = description;
+        this.name = name;
+        this.status = status;
+    }
+    public Task (String name, String description, TaskStatus status, int id){
+        this.description = description;
+        this.name = name;
+        this.status = status;
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return name.equals(task.name) && description.equals(task.description) &&
+                status.equals(task.status) && id == task.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, description, status, id);
+    }
+
+    @Override
+    public String toString() {
+        return "tasks.Task{" +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", id=" + id +
+                ", status=" + status +
+                '}';
+    }
+}
